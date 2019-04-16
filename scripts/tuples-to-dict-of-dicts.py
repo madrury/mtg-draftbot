4c4679d8a480147aaa7c0380940a3de91bd0b020
madrury <matthew.drury.83@gmail.com>
2019-04-15 19:41:59 -0700

    Finish creating m19 card value data.

---
+++ scripts/tuples-to-dict-of-dicts.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+j = json.load(sys.stdin)
+
+archytype_names = j['archytype_names']
+archytype_values = j['values']
+
+card_values = {}
+for name, values in archytype_values.items():
+    card_values[name] = dict(zip(archytype_names, values))
+
+json.dump(card_values, sys.stdout)
