b27a5b4326b468ed73ce432a499564d6306dfd6a
madrury <matthew.drury.83@gmail.com>
2019-05-28 21:19:15 -0700

    Fix data layout of reduced set.

--- scripts/subset-card-set.py
+++ scripts/subset-card-set.py
@@ -6,9 +6,9 @@ card_values = json.load(open(sys.argv[2]))
 
 card_names_to_keep = set(card_values)
 
-reduced_set = {}
+reduced_set = [] 
 for card in card_set:
     if card['name'] in card_names_to_keep:
-        reduced_set[card['name']] = card.copy()
+        reduced_set.append(card.copy())
 
 json.dump(reduced_set, sys.stdout)
