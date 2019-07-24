4167f480045ba0179caf342103e6c7527ad2d1fd
madrury <matthew.drury.83@gmail.com>
2019-07-24 13:17:23 -0700

    Add documentation to analytic table creation code.

--- make_analytic_table.py
+++ make_analytic_table.py
@@ -9,33 +9,60 @@ TABLE_NAMES = {'preferences', 'options', 'picks', 'cards'}
 
 
 class AnalyticTableConstructor:
+    """Construct an analytic base table for learning draft archetype weights
+    from draft pick data.
 
+    Parameters
+    ----------
+    db_path: Union[str, Path]
+      Path to an sqlite3 data base containing draft data.
+
+    Given this database, the make_analytic_base_table method constructs
+    analytic style data useful for machine learning.
+    """
     def __init__(self, db_path):
         self.conn = sqlite3.connect(db_path)
         self.cursor = self.conn.cursor()
         self.validate_db()
 
-    def validate_db(self):
-        self.cursor.execute(
-            "SELECT name FROM sqlite_master WHERE type='table';")
-        # We've got to unpack some singleton tuples here.
-        table_names = {t[0] for t in self.cursor.fetchall()}
-        assert table_names == TABLE_NAMES
-
     def make_analytic_base_table(self):
+        """Construct analytic machine learning data from the tables in the
+        database.
+
+        Returns
+        -------
+        X: pd.DataFrame
+          Data frame indexed by (draft_id, drafter_id, pick_number) containing
+          features known at the time of a given pick in a draft, intended to be
+          used as predictors for the next pick. There predictors come in two types:
+            - The options for picks from the current pack.
+            - The cards held by the current drafter (from past picks).
+        
+        y: pd.Series
+          Data series indexed by (draft_id, drafter_id, pick_number) containing
+          the pick made by the drafter, intended to be a used as a target for a
+          predictive model.
+        """
         options = self.fetch_table('options',
                                    table_validator=validate_options,
                                    name_transformer=name_sanitizer('options'))
         cards = self.fetch_table('cards',
                                  table_validator=validate_cards,
                                  name_transformer=name_sanitizer('cards'))
+        X = pd.merge(options, cards, left_index=True, right_index=True)
         picks = self.fetch_table('picks',
                                  table_validator=validate_picks)
-        pick_idx = pd.Series(np.argmax(picks.values, axis=1), 
-                             index=picks.index)
-        joined = pd.merge(options, cards, left_index=True, right_index=True)
-        joined["pick"] = pick_idx
-        return joined
+        y = pd.Series(np.argmax(picks.values, axis=1), 
+                          index=picks.index)
+        return X, y
+
+    def validate_db(self):
+        """Make sure that the database contains the needed tables."""
+        self.cursor.execute(
+            "SELECT name FROM sqlite_master WHERE type='table';")
+        # We've got to unpack some singleton tuples here.
+        table_names = {t[0] for t in self.cursor.fetchall()}
+        assert table_names == TABLE_NAMES
 
     def fetch_table(self, table_name, *,
                     table_validator=None,
