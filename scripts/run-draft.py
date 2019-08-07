36c7906c2c8b15fa1f85fb84f680f3ed3326bdc9
madrury <matthew.drury.83@gmail.com>
2019-08-06 21:45:57 -0700

    Move run draft script.

--- scripts/run-draft.py
+++ scripts/run-draft.py
@@ -9,7 +9,7 @@ for n in range(n_drafts):
     if n % 100 == 0:
         print(f"Draft {n}.")
     draft = Draft(
-        cards_path='data/m19-subset-reduced.json',
-        card_values_path='data/m19-custom-card-values-reduced.json')
+        cards_path='data/m20/m20-cards-subset.json',
+        card_values_path='data/m20/m20-default-weights-subset.json')
     draft.draft()
     draft.write_to_database(db_path)
