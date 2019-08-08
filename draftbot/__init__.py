eb8df1def373062087452334eb624c50ae41c371
madrury <matthew.drury.83@gmail.com>
2019-08-07 20:08:54 -0700

    Fix broken import in __init__.py

--- draftbot/__init__.py
+++ draftbot/__init__.py
@@ -1,4 +1,4 @@
 from .draftbot_learn import DraftBotModel, DraftBotModelTrainer
 from .draftbot_sim import Draft, Set
-from .plotting import 
+from .plotting import DraftPlotter
 from .make_analytic_table import AnalyticTableConstructor
