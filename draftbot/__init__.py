cd2461f23403b28e7f9d44e5c4b3e66dd23904a5
madrury <matthew.drury.83@gmail.com>
2019-08-07 17:52:19 -0700

    Add plotting code import to __init__.py

--- draftbot/__init__.py
+++ draftbot/__init__.py
@@ -1,3 +1,4 @@
 from .draftbot_learn import DraftBotModel, DraftBotModelTrainer
 from .draftbot_sim import Draft, Set
+from .plotting import 
 from .make_analytic_table import AnalyticTableConstructor
